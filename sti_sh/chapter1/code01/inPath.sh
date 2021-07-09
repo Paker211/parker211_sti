d3a2992714058c26fd4f6deb85f58f9a5d27b25f
Parker211 <parker40105@gmail.com>
2021-07-09 23:58:23 +0800

    Jul_9_2021

--- sti_sh/chapter1/code01/inPath.sh
+++ sti_sh/chapter1/code01/inPath.sh
@@ -1,2 +1,56 @@
 #! /bin/bash
 # inPath
+
+function inpath(){
+  cmd=$1
+  path=$2
+  retval=1
+  oldIFS=$IFS
+  IFS=":"
+
+  for directory in $path
+  do
+    if [ -x $dirctory/$cmd ] ; then
+      retval=0
+    fi
+  done
+
+  IFS=$oldIFS
+  return $retval
+
+}
+
+function checkForCmdInPath(){
+  var=$1
+
+  if [ "$var" != "" ] ; then
+    if [ "${var%${var#?}}" = "/" ] ; then
+      if [ ! -x $var ] ; then
+        return 1
+      fi
+    elif ! inpath $var $PATH ; then
+      return 2
+    fi
+  fi
+}
+
+
+# Main
+
+if [ $# -ne ] ; then
+  echo "Usage: $0 command" >&2
+  exit 1
+fi
+
+checkForCmdInPath "$1"
+
+case $? in
+  0 ) echo "$1 found in PATH"  ;;
+  1 ) echo "$1 not found or not executable"  ;;
+  2 ) echo "$1 not found in PATH"
+esac
+
+exit 0
+
+
+
---
+++ sti_sh/chapter6/test.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+for i in {49..57}
+do
+  mkdir code${i}
+done
+
+
