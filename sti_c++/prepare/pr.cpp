c9a5c11acd8e9de85921a70b786af4e3d852a837
Parker211 <parker40105@gmail.com>
2021-09-04 23:42:11 +0800

    practice c++

--- sti_c++/prepare/pr.cpp
+++ sti_c++/prepare/pr.cpp
@@ -1,19 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-  int num, tmp ;
+int main(){
+  int insertNum ;
   vector<int> v ;
-  cin >> num ;
-  for(int i=0; i<num ;i++) {
-    cin >> tmp ;
-    v.push_back( tmp ) ;
-  }
-  sort(v.begin(), v.end()) ;
+  int erasePosition ;
 
-  for(int i=0; i<num; i++) {
+  cin >> insertNum ;
+  int temp ;
+  while( insertNum-- ) {
+    cin >> temp ;
+    v.push_back( temp ) ;
+  }
+
+  cin >> erasePosition ;
+  v.erase( v.begin()+erasePosition-1 ) ;
+
+  int rangeStart, rangeEnd ;
+  cin >> rangeStart >> rangeEnd ;
+  for(int i=rangeStart; i<rangeEnd; i++) {
+    v.erase( v.begin()+rangeStart-1 ) ;
+  }
+
+  cout << v.size() << endl ;
+  for(int i=0 ;i<v.size() ;i++) {
     cout << v[i] << " " ;
   }
   cout << endl ;
 
+
 }
+
