2db5a76a2a81379a527143e41e062433cf53a517
Parker211 <parker40105@gmail.com>
2021-09-07 22:37:26 +0800

    pactice c++

--- sti_c++/learn/objectOriented/inheritance/pr.cpp
+++ sti_c++/learn/objectOriented/inheritance/pr.cpp
@@ -1,6 +1,45 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+class Shape {
+  protected :
+    int width ;
+    int height ;
+  public :
+    void setWidth(int w) {
+      width = w ;
+    }
+    void setHeight(int h) {
+      height = h ;
+    }
+};
 
+class paintCost {
+  public :
+    int getCost(int area) {
+      return area*70 ;
+    }
+};
+
+class Rectangle: public Shape, public paintCost {
+  public :
+    int getArea() {
+      return (width*height) ;
+    }
+};
+
+
+int main() {
+  Rectangle rect ;
+  int area ;
+
+  rect.setWidth( 5 ) ;
+  rect.setHeight( 7 ) ;
+
+  area = rect.getArea() ;
+
+  cout << "Total area: " << rect.getArea() << endl ;
+  cout << "Total paint cost: $" << rect.getCost( area ) << endl ;
+
+  return 0 ;
 }
--- sti_c++/learn/objectOriented/overloading/pr.cpp
+++ sti_c++/learn/objectOriented/overloading/pr.cpp
@@ -1,6 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+class printData {
+  public :
+    void print( int i ) {
+      cout << "Printing int: " << i << endl ;
+    }
+    void print( double f ) {
+      cout << "Printing float: " << f << endl ;
+    }
+    void print( string s ) {
+      cout << "Printing string: " << s << endl ;
+    }
+};
 
+int main() {
+  printData pd ;
+
+  pd.print( 5 ) ;
+
+  pd.print( 500.234 ) ;
+
+  pd.print( "hello world" ) ;
+
+  return 0 ;
 }
