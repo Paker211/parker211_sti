67cde855d5710c7b99a58ac558a1d6d3b4c45fc4
Parker211 <parker40105@gmail.com>
2022-01-19 23:10:17 +0800

    proactice vue project with basic syntax

---
+++ sti_py/test/pr.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+a = [1, 2, 3]
+print(a)
+print(type(a))
+
---
+++ sti_vue/tutorial/demo4/demo4/src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <h1>{{ title }}</h1>
+  <p>Welcome...</p>
+  <div v-if="showModal">
+    <Modal theme="dfsale" @close="toggleModal" >
+      <h1>Ninja Giveaway!!</h1>
+      <p>Grab your ninja swag for half price!</p>
+    </Modal>
+  </div>
+  <button @click="toggleModal">open modal</button>
+</template>
+
+<script>
+import Modal from './components/Modal.vue'
+
+export default {
+  name: 'App',
+  components: { Modal },
+  data() {
+    return {
+      title: 'My first Vue App :))',
+      header: 'Sign up for the Giveaway!!',
+      text: 'Grab your ninja swag for half price!',
+      showModal: false,
+    }
+  },
+  methods: {
+    toggleModal() {
+      this.showModal = !this.showModal
+
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+h1 {
+  border-bottom: 1px solid #ddddff;
+  display: inline-block;
+  padding-bottom: 10px;
+}
+.modal.sale {
+  background: crimson;
+  color: white;
+}
+.modal.sale h1 {
+  color: white;
+}
+</style>
