e8936517530857257149e806d30cf02d99123455
Parker211 <parker40105@gmail.com>
2022-02-16 20:20:54 +0800

    practice vue3+ts

--- sti_vue/tutorial/typeScript/hyrule-job/src/App.vue
+++ sti_vue/tutorial/typeScript/hyrule-job/src/App.vue
@@ -1,35 +1,71 @@
 <template>
   <div class="app">
-    hello, hyrule.
-    <p>{{ name }} - {{ age }}</p>
-    <button @click="changeName('Zico')">changeName</button>
-    <button @click="changeAge('40')">changeAge</button>
+    <header>
+      <div class="title">
+        <img src="./assets/butterfly.svg" alt="logo">
+        <h1>Hyrule Jobs</h1>
+      </div>
+      <div class="order">
+        <button @click="handleClick('title')">order by title</button>
+        <button @click="handleClick('salary')">order by salary</button>
+        <button @click="handleClick('location')">order by location</button>
+      </div>
+    </header>
+    <JobList :jobs="jobs" :order="order"/>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
+import JobList from './components/JobsList.vue'
+import Job from './types/Job';
+import OrderTerm from './types/OrderTerm';
 
 export default defineComponent({
   name: 'App',
-  components: {},
-  data() {
-    return {
-      name: 'Link',
-      age: 23 as number | string,
-    }
-  },
-  methods: {
-    changeName(name: string) {
-      this.name = name ;
-    },
-    changeAge(age: number | string) {
-      this.age = age ;
+  components: { JobList },
+  setup() {
+    const jobs = ref<Job[]>([
+      { title: 'farm worker', location: 'lon lon ranch', salary: 30000, id: '1' },
+      { title: 'quarryman', location: 'death mountain', salary: 40000, id: '2' },
+      { title: 'flute player', location: 'the lost woods', salary: 35000, id: '3' },
+      { title: 'fisherman', location: 'lake hylia', salary: 21000, id: '4' },
+      { title: 'person guard', location: 'gerudo valley', salary: 32000, id: '5' }
+    ])
+    const order = ref<OrderTerm>('title')
+
+    const handleClick = (term: OrderTerm) => {
+      order.value = term
     }
 
+    return { jobs, handleClick, order }
   }
 });
 </script>
 
 <style>
+header {
+  text-align: center;
+}
+header.order {
+  margin-top: 20px;
+}
+button {
+  margin: 0 10px;
+  color: #1195c9;
+  border: 3px solid #1195c9;
+  background: #d5f0ff;
+  padding: 8px 16px;
+  border-radius: 4px;
+  cursor: pointer;
+  font-weight: bold;
+}
+header .title {
+  display: flex;
+  justify-content: center;
+}
+header img {
+  width: 60px;
+  margin-content: center;
+}
 </style>
